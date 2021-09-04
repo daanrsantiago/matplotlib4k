24aaae5398d21a989ea29eafb7a990bdedf7c27f
Daniel Ribeiro Santiago <danielribeirosantiago.tucano@gmail.com>
2021-09-04 18:03:30 -0300

    Changes grid options enum names

--- src/main/kotlin/matplotlib/pyplot/grid.kt
+++ src/main/kotlin/matplotlib/pyplot/grid.kt
@@ -6,11 +6,11 @@ import python.PythonScriptBuilder
 
 fun grid(
     b: Boolean? = null,
-    which: gridWhich = gridWhich.major,
-    axis: gridAxis = gridAxis.both
+    which: GridWhichOptions = GridWhichOptions.major,
+    axis: GridAxisOptions = GridAxisOptions.both
 ) {
     PythonScriptBuilder.addCommand("plt.grid(b=${b.toPythonBooleanOrNone()},which=${which.toPythonStringQuotedOrEmpty()},axis=${axis.toPythonStringQuotedOrEmpty()})")
 }
 
-enum class gridWhich { major, minor, both }
-enum class gridAxis { both, x, y }
+enum class GridWhichOptions { major, minor, both }
+enum class GridAxisOptions { both, x, y }
