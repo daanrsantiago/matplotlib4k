cbd770d554b79dfa7572d92d6dbc0e07737d6382
Daniel Ribeiro Santiago <danielribeirosantiago.tucano@gmail.com>
2021-09-04 18:03:04 -0300

    Fixes colorExtensions to generate correct python color tuples

--- src/main/kotlin/extensions/colorExtensions.kt
+++ src/main/kotlin/extensions/colorExtensions.kt
@@ -3,5 +3,5 @@ package matplotlib
 import java.awt.Color
 
 fun Color?.toPythonTupleString():String {
-    return if (this == null) "None" else "(${this.red},${this.green},${this.blue},${this.alpha})"
+    return if (this == null) "None" else "(${this.red/255},${this.green/255},${this.blue/255},${this.alpha/255})"
 }
