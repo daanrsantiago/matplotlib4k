d87b1aa97bbf1907582c42318bac45a674001f6b
Daniel Ribeiro Santiago <danielribeirosantiago.tucano@gmail.com>
2021-09-04 17:52:46 -0300

    Figure now implements PythonVariable interface

--- src/main/kotlin/matplotlib/Figure.kt
+++ src/main/kotlin/matplotlib/Figure.kt
@@ -5,10 +5,11 @@ import extensions.toPythonStringQuotedOrEmpty
 import extensions.toPythonStringQuotedOrNone
 import matplotlib.Axes3D.Companion.axes3DNumber
 import python.PythonScriptBuilder
+import python.PythonVariable
 
-interface Figure {
+interface Figure: PythonVariable {
 
-    val figureName: String
+    override val variableName: String
 
     companion object {
         var figureNumber: Int = 0
@@ -18,34 +19,46 @@ interface Figure {
             private set
     }
 
+    enum class FigureArgs : KwargKey {
+        figsize, dpi, faceColor, edgeColor, lineWidth,
+        frameOn, tightLayout, constrainedLayout
+    }
+
     // TODO add_subplot(projection='3d') -> Should return Axis3D
 
-    fun add_suplot(projection: AddSubplotProjection? = null): Axes3D {
-        return object: Axes3D {
-            override val axes3DName: String = "axes3d_$axes3DNumber"
+    fun add_subplot(projection: AddSubplotProjectionOptions? = null): Axes3D {
+        return object : Axes3D {
+            override val variableName: String = "axes3d_$axes3DNumber"
+
             init {
-                PythonScriptBuilder.addCommand("$axes3DName = $figureName.add_subplot(" +
-                        "projection=${projection.toPythonStringQuotedOrNone()}" +
-                        ")")
+                PythonScriptBuilder.addCommand(
+                    "$variableName = ${this@Figure.variableName}.add_subplot(" +
+                            "projection=${projection.toPythonStringQuotedOrNone()}" +
+                            ")"
+                )
             }
         }
     }
 
-    enum class AddSubplotProjection {aitoff, hammer, lambert, mollweide, polar, rectilinear, `3d`}
+    enum class AddSubplotProjectionOptions { aitoff, hammer, lambert, mollweide, polar, rectilinear, `3d` }
 
     fun show() {
-        PythonScriptBuilder.addCommand("$figureName.show()")
+        PythonScriptBuilder.addCommand("$variableName.show()")
     }
 
     /**
      *  Figure.savefig(self, fname, *, transparent=None, **kwargs):
      */
-    fun saveFig(fname: String, transparent: Boolean = false, saveFigKwargs: Map<SaveFigKwargs, KwargValue>? = null) {
-        PythonScriptBuilder.addCommand("$figureName.savefig(${fname.toPythonStringQuotedOrEmpty()}, transparent=${transparent.toPythonBooleanOrNone()} ${saveFigKwargs.toKwargPythonStringOrEmpty()} )")
+    fun saveFig(
+        fname: String,
+        transparent: Boolean = false,
+        saveFigKwargs: Map<SaveFigKwargsKeys, KwargValue>? = null
+    ) {
+        PythonScriptBuilder.addCommand("$variableName.savefig(${fname.toPythonStringQuotedOrEmpty()}, transparent=${transparent.toPythonBooleanOrNone()} ${saveFigKwargs.toKwargPythonStringOrEmpty()} )")
     }
 
-    enum class SaveFigKwargs: KwargKey {
+    enum class SaveFigKwargsKeys : KwargKey {
         dpi, quality, optimize, progressive, facecolor, edgecolor, orientation, papertype, format, bbox_inches,
-                pad_inches, bbox_extra_artists, backend, metadata, pil_kwargs
+        pad_inches, bbox_extra_artists, backend, metadata, pil_kwargs
     }
 }
