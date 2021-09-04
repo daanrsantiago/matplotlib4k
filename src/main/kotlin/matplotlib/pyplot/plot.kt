e6c3d1cb008fd53c368f9356281dc726f43d6498
Daniel Ribeiro Santiago <danielribeirosantiago.tucano@gmail.com>
2021-09-04 18:05:34 -0300

    Adds kwargs option to plot function, fixes some problems and reflect introduction of PythonVariable class

--- src/main/kotlin/matplotlib/pyplot/plot.kt
+++ src/main/kotlin/matplotlib/pyplot/plot.kt
@@ -1,23 +1,27 @@
 package matplotlib.pyplot
 
 import extensions.emptyIfNullOrComma
-import extensions.toPythonNumberArrayString
+import extensions.toPythonNumberArrayStringOrEmpty
 import extensions.toPythonStringQuotedOrEmpty
+import matplotlib.KwargValue
 import matplotlib.Line2D
+import matplotlib.toKwargPythonStringOrEmpty
 import python.PythonScriptBuilder
 
 fun plot(
     x: List<Double>? = null,
     y: List<Double>,
-    fmt: String? = null
+    fmt: String? = null,
+    kwargs: Map<Line2D.Line2DArgs, KwargValue>? = null
 ): Line2D {
     return object: Line2D {
-        override val line2DName: String = "line2D_${Line2D.line2DNumber}"
+        override val variableName: String = "line2D_${Line2D.line2DNumber}"
         init {
-            PythonScriptBuilder.addCommand("$line2DName = plt.plot(" +
-                    "${x.toPythonNumberArrayString()}${x.emptyIfNullOrComma()}" +
-                    "${y.toPythonNumberArrayString()}${fmt.emptyIfNullOrComma()}" +
-                    "${fmt.toPythonStringQuotedOrEmpty()}" +
+            PythonScriptBuilder.addCommand("$variableName = plt.plot(" +
+                    "${x.toPythonNumberArrayStringOrEmpty()}${x.emptyIfNullOrComma()}" +
+                    "${y.toPythonNumberArrayStringOrEmpty()}${fmt.emptyIfNullOrComma()}" +
+                    "${fmt.toPythonStringQuotedOrEmpty()}${kwargs.emptyIfNullOrComma()}" +
+                    kwargs.toKwargPythonStringOrEmpty() +
                     ")")
         }
     }
